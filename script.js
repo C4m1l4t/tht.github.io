1ccb67589e9510edd7905cdb71d84465a05a18ac
C4m1l4t <camilanicole1.torres@gmail.com>
2023-10-15 03:02:30 -0300

    thtv2

---
+++ script.js
@@ -0,0 +1,18 @@
+let boton = document.querySelector("#tema");
+let body = document.querySelector("body");
+let darkMode = false;
+boton.addEventListener("click",()=>{
+    if (darkMode == false){
+        body.style.backgroundColor = "black";
+        darkMode = true;
+    }else{
+        body.style.backgroundColor = "white";
+        darkMode = false;
+    }
+})
+let textos = document.querySelectorAll(".titulo-fotos")
+
+         body.style.backgroundColor = "black";
+         for (let i = 0; i < textos.length; i++){
+             textos[i].style.color = "white";
+         }
